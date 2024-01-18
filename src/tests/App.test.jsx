73531444931efda6d32ccf483323fd945837ae98
rewqla <desares526@gmail.com>
2024-01-18 15:09:30 +0200

    Add test for displaying a saved color

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,13 +1,18 @@
-import { screen, waitFor } from "@testing-library/react";
+import { screen, waitFor, fireEvent } from "@testing-library/react";
 import App from "../App";
 import { renderWithProviders } from "../utils/renderWithProviders";
 import user from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
-test("It should save color after clicking Save Color button", async () => {
+test("It should save color after clicking the Save Color button", async () => {
   renderWithProviders(<App />);
+
   const noSavedColors = await screen.findByText(/No saved colors yet./i);
   expect(noSavedColors).toBeInTheDocument();
+
+  fireEvent.change(await screen.findByLabelText("red"), {
+    target: { value: "123" },
+  });
 
   const saveColorButton = await screen.findByRole("button", {
     name: /Save Color/i,
@@ -20,7 +25,28 @@ test("It should save color after clicking Save Color button", async () => {
   expect(noSavedColors).not.toBeInTheDocument();
 });
 
-test("It should remove after clicking Delete button", async () => {
+test("It should display saved color after clicking the Display color button", async () => {
+  renderWithProviders(<App />);
+
+  const initialBackgroundColor = (await screen.findByTestId("color-preview"))
+    .style.backgroundColor;
+
+  const displayColorButton = await screen.findByRole("button", {
+    name: /Display color/i,
+  });
+
+  await waitFor(async () => {
+    await user.click(displayColorButton);
+  });
+  await waitFor(async () => {
+    const newBackgroundColor = (await screen.findByTestId("color-preview"))
+      .style.backgroundColor;
+
+    expect(initialBackgroundColor).not.toBe(newBackgroundColor);
+  });
+});
+
+test("It should remove after clicking the Delete button", async () => {
   renderWithProviders(<App />);
 
   const initialSavedColors = await screen.findAllByRole("listitem");
